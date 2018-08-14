9527c22e3a8cb2af11518f7fd9fa112d43df6997
陈键钊 <chenjz@77ircloud.com>
2018-08-14 09:00:47 +0800

    feat: 抽取 pipeline

--- vars/main.groovy
+++ vars/main.groovy
@@ -3,7 +3,13 @@
  * 2018/8/13
  */
 
-def call() {
+def call(body) {
+
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
 
     pipeline {
         agent any
