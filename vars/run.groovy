153d4c37c0fb973b509fb69ab993a2837950ceff
陈键钊 <chenjz@77ircloud.com>
2018-08-14 09:39:02 +0800

    fix: rename pipeline 入口方法

--- vars/run.groovy
+++ vars/run.groovy
@@ -5,12 +5,6 @@
 
 def call(body) {
 
-    // evaluate the body block, and collect configuration into the object
-    def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
-
     pipeline {
         agent any
 
